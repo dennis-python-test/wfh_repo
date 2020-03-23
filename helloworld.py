d63b522bda516673107e7f1b4add0209ee14634f
dennis-python-test <62574510+dennis-python-test@users.noreply.github.com>
2020-03-23 15:20:11 -0700

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def hello(int):
+    """Print "Hello World" and return None."""
+    print((int), "Hello World")
+
+def repeatHello():
+    
+    for i in range (0, 5):
+        hello(i)
+    #    print str(" ") + hello())
+    
+    
+repeatHello()
